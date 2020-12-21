9a142bd5ebf333eabe47a55cc915a522bd930296
anijjar <anijjar@uvic.ca>
2020-12-21 02:27:32 -0800

    remade the gui

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Learning In Motion - Co-op posting Scrapper
+# Learning In Motion - Co-op Posting Scrapper
 
 This here is a simple Python script that will gather job postings from the University of Victoria's Job Bullin board on Learning in Motion. This script will break if they decide to make major changes to the source code, but I doubt it. Although in that case, the script will need to be updated.  
 
@@ -8,5 +8,5 @@ In the future, I hope to implement features such as:
 - Submit applications via gui.
 - Anything else I can think of.
 
-Feel free to modify this as you wish. Currently, I am facing issues with pyinstaller so the only way to run this is to pass it through an interpreter or IDE like VS code and run it manually. Linux users can make this script into an exe by "chmod +x app.py" and be done with it. 
+Feel free to modify this as you wish. Currently, I am facing issues with pyinstaller so no nice executables, but you can run it as a regular script via "python app.py"
 
--- app.py
+++ app.py
@@ -1,66 +1,117 @@
 #!/usr/bin/env python
-import tkinter as tk
+from tkinter import *
+import sys
+import os
 import subprocess
+import threading
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import selenium.webdriver.support.ui as ui
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
-    
+
+first_iteration = True
+##
+#       Known Bugs
+# - Will skip the last posting always
+# - Occasionally stop at a few postings
+# - Cannot hide the browser using the headless command
+# - 
+#       Things to fix/add
+# - join the secondary thread with the main one when the quit or x button is pressed
+# - add some details to the toolbar to make it look legit
+# - choose some color pallet
+# - have it save to a different location
+#    - run the organize script or print script or etc. 
+##      
+class Toolbar:
+    def __init__ (self, _win):
+        self.rt = _win
+        self.menubar = Menu(self.rt)
+        self.rt.config(menu=self.menubar)
+
+        fileMenu = Menu(self.menubar, tearoff=False)
+        editMenu = Menu(self.menubar, tearoff=False)
+        viewMenu = Menu(self.menubar, tearoff=False)
+        helpMenu = Menu(self.menubar, tearoff=False)
+        fileMenu.add_command(label="Exit", command=quit)
+        helpMenu.add_command(label="Info", command=self.modal_info)
+        fileMenu.add_command(label="Print", command=quit)
+        self.menubar.add_cascade(label="File", menu=fileMenu)
+        self.menubar.add_cascade(label="Edit", menu=editMenu)
+        self.menubar.add_cascade(label="View", menu=viewMenu)
+        self.menubar.add_cascade(label="Help", menu=helpMenu)
+
+    def modal_info(self):
+        Win2 = Toplevel(self.rt)
+        Win2.configure(bg='#bebebe')
+        Win2.title("Info Menu")
+        Win2.iconbitmap(r"./assets/logo.ico")
+        Win2.greeting = Label(Win2, text="Motivation", bg='#bebebe',fg='#444444')
+        Win2.message = Label(Win2, text="Hope this app helps you as it helped me. \nNot sure if UVic will get angry about something like \nthis being public knowledge, but if anyone has a problem, email me at \nanijjar@uvic.ca and I will take it down. If anyone wants to hang out, \nhmu or checkout AUVIC. All the best!", bg='#bebebe',fg='#444444')
+        Win2.greeting.config(font=("Courier", 12, 'bold'))
+        Win2.message.config(font=("courier", 11))
+        Win2.greeting.pack(padx=10,pady=(5,0))
+        Win2.message.pack(padx=10,pady=(5,0))
+     
 class Main:
-    def __init__ (self, root):
-        self.root = root
-        self.root.title("LIM-scrapper")
-        self.root.iconbitmap(r"./assets/logo.ico")
-        self.root.geometry("250x150")
-        self.root.resizable(width=False, height=False)
-        frm_input = tk.Frame(master=self.root)
-        self.root.lbl_netlinkID = tk.Label(
-            master=frm_input, 
-            text="Netlink ID:"
-        )
-        self.root.ent_netlinkID = tk.Entry(
-            master=frm_input
-        )
-        self.root.lbl_password = tk.Label(
-            master=frm_input, 
-            text="Password:"
-        )
-        self.root.ent_password = tk.Entry(
-            master=frm_input, 
-            show="*"
-        )
+    def __init__ (self, _win):
+        self.rt = _win
+        self.rt.configure(bg='#f4d476')
+        self.rt.bind('<Return>', self.threadExecute)
+        self.rt.bind('<Escape>', self.exit_app)
 
-        frm_btns = tk.Frame(master=self.root)
-        self.root.btn_quit = tk.Button(
-            master=frm_btns,
-            text="QUIT",
-            fg="red",
-            command=quit
-        )       
-        self.root.btn_getAllPDFs = tk.Button(
-            master=frm_btns,
-            text="Download All PDFs",
-            fg="green",
-            command=self.execute
-        )
-        self.root.lbl_netlinkID.pack()
-        self.root.ent_netlinkID.pack()
-        self.root.lbl_password.pack()
-        self.root.ent_password.pack()
-        frm_input.grid(row=0,column=0,padx=10)
-
-        self.root.btn_quit.grid(row=0,column=0,padx=5)
-        self.root.btn_getAllPDFs.grid(row=0,column=1,padx=5)
-        frm_btns.grid(row=1,column=0,padx=10)
+        self.rt.greeting = Label(self.rt, text="Enter your credentials", bg='#bebebe', fg='#444444')
+        self.rt.greeting.config(font=("Courier", 12, 'bold'))
+        self.rt.greeting.pack(padx=10,pady=(5,0))
         
-    def execute (self):
-        global netlinkID = self.root.ent_netlinkID.get()
-        global password = self.root.ent_password.get()
+        self.rt.netlinkID = Entry(self.rt)
+        self.rt.netlinkID.insert(0,'--NetlinkID--')
+        self.rt.netlinkID.pack(padx=10,pady=(5,0))
+        self.rt.netlinkID.bind("<FocusIn>", lambda args: self.rt.netlinkID.delete('0', 'end'))
+        
+        self.rt.Password = Entry(self.rt,show="*")
+        self.rt.Password.insert(0,'--Password--')
+        self.rt.Password.pack(padx=10,pady=(5,0))
+        self.rt.Password.bind("<FocusIn>", lambda args: self.rt.Password.delete('0', 'end'))
+
+        self.rt.frm1 = Frame(self.rt, bg='#bebebe')
+        self.rt.frm1.pack(padx=10,pady=(5,0))
+        self.rt.btn_quit = Button(self.rt.frm1, text="   Quit   ", command=self.exit_app)
+        self.rt.btn_go = Button(self.rt.frm1, text=" Execute  ", command=self.threadExecute)
+        self.rt.btn_quit.grid(column=0, row=0,padx=8,pady=(5,5))
+        self.rt.btn_go.grid(column=1, row=0,padx=8,pady=(5,5))
+        
+        self.rt.h1 = Label(self.rt, text="Job Postings Obtained", bg='#bebebe',fg='#444444')
+        self.rt.h1.config(font=("Courier", 12))
+        self.rt.h1.pack(padx=10,pady=(5,0))
+        
+        self.rt.frm2 = Frame(self.rt) #show job postings collected after execute
+        self.rt.frm2.pack(padx=10,pady=(5,10))
+        self.rt.scroll = Scrollbar(self.rt.frm2, orient=VERTICAL)
+        self.rt.select = Listbox(self.rt.frm2, yscrollcommand=self.rt.scroll.set, height=8)
+        self.rt.scroll.config(command=self.rt.select.yview)
+        self.rt.scroll.pack(side=RIGHT, fill=Y)
+        self.rt.select.pack(side=LEFT, fill=BOTH, expand=1)
+
+
+    def threadExecute(self, event=None):
+        t = threading.Thread(target=self.execute).start()
+        #update list
+
+   
+    #get job postings from LIM
+    def execute(self):
+        print("Getting data from website")
+
+        netlinkID = self.rt.netlinkID.get()
+        password = self.rt.Password.get()
+
         #######################################
         #  UVic - Student Login Screen
         #######################################
         #Create a webdriver instance
-        driver = webdriver.Chrome('./driver/chromedriver.exe')
+        driver = webdriver.Chrome(executable_path='./driver/chromedriver.exe')
         # Starting point - login as a student
         URL = 'https://learninginmotion.uvic.ca/students/NetlinkID/student-login.htm'
         #Go to page in URL
@@ -90,15 +141,15 @@ class Main:
         URL = driver.current_url
 
         # click on search postings
-        elem_SearchPostings = driver.find_element_by_link_text("Search Postings").click()
+        driver.find_element_by_link_text("Search Postings").click()
         driver.implicitly_wait(0.5)
 
         # click on co-op postings
-        elem_SearchPostings_CoopPostings = driver.find_element_by_link_text("Co-op postings").click()
-        driver.implicitly_wait(1.5)
+        driver.find_element_by_link_text("Co-op postings").click()
+        driver.implicitly_wait(0.5)
 
         # click on "all postings" button
-        elem_Postings = driver.find_element_by_xpath("//input[@type='submit']").click()
+        driver.find_element_by_xpath("//input[@type='submit']").click()
         driver.implicitly_wait(0.5)
 
         # Make a list of all job listings
@@ -115,27 +166,96 @@ class Main:
             tdTagList = listing.find_elements_by_tag_name("td")[3]
             title = tdTagList.get_attribute("data-totitle")
             # find the span element that contains the link to the posting via "title"
-            link = listing.find_element_by_xpath("//span[@title='"+title+"']")
-            link.click()
+            listing.find_element_by_xpath("//span[@title='"+title+"']").click()
             driver.switch_to.window(driver.window_handles[1])
 
             # click print
-            driver.implicitly_wait(0.5)
-            elem_PrintPDF = driver.find_element_by_link_text("Print").click()
+            driver.implicitly_wait(0.2)
+            driver.find_element_by_link_text("Print").click()
+            driver.implicitly_wait(5)
+
+            # add the name of the file to a global list
+
             #close tab
             driver.close()
             driver.switch_to.window(p)
-            driver.implicitly_wait(0.8)
+            driver.implicitly_wait(0.5)
+
         #close session
         driver.close()
-        
-    def get_driver(self,driver="chrome"):
+            
+    
+    #delete selected job applications
+    def delete(self):
+        print("the delete button has been pressed")
+    
+    #modal of message
+    def error_msg(self,msg):
+        #make new window
+        #add new label
+        #add a close button
         pass
+    
+    def exit_app(self,event=None):
+        sys.exit()
+
+    #adds pdf found to listbox
+    def add_to_listBox(self):    
+        pass
+
+def CENTER_DEBUG_SCREEN(argument):
+    # Gets the requested values of the height and widht.
+    windowWidth = argument.winfo_reqwidth()
+    windowHeight = argument.winfo_reqheight()
+    
+    # Gets both half the screen width/height and window width/height
+    positionRight = int(argument.winfo_screenwidth()/2 - windowWidth/2)
+    positionDown = int(argument.winfo_screenheight()/2 - windowHeight/2)
+    
+    # Positions the window in the center of the page.
+    argument.geometry("+{}+{}".format(positionRight, positionDown))
+
+class MainApplication:
+    def __init__ (self, _win):
+        self.rt = _win
+        self.rt.title("LIM-scrapper")
+        self.rt.iconbitmap(r"./assets/logo.ico")
+        self.rt.resizable(width=False, height=False)
+        #initialize toolbar
+        Toolbar(self.rt)
+        #initlaize Main screen
+        Main(self.rt)
+        #clear the console for debugging
+        CENTER_DEBUG_SCREEN(self.rt)
         
-    def start_batch(): 
-       subprocess.call([r'C:\Users\Ron\Desktop\Run Batch\Matrix.bat'])
-       
+    
 if __name__ == '__main__':
-    root = tk.Tk()
-    obj = Main(root)
-    root.mainloop()
+    os.system('CLS')
+    win = Tk()
+    MainApplication(win)
+    win.mainloop()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
